fe7aee3cb39dcd6c2420a6e7d03e50974008f7e8
pazzfj <01121697@yto.net.cn>
2017-12-29 14:15:26 +0800

    new version

---
+++ config-client-bus/src/main/java/com/example/configclientbus/ConfigClientBusApplication.java
@@ -0,0 +1,26 @@
+package com.example.configclientbus;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class ConfigClientBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientBusApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
+	}
+
+}
